4a48e97a98c0afe354f349d745e4daa4cfc15f0f
Sina Sadigh <42268464+sinasadigh@users.noreply.github.com>
2023-09-24 17:34:54 +0300

    Update index.ts

--- auth/src/router/index.ts
+++ auth/src/router/index.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response, NextFunction, ErrorRequestHandler} from "express";
 const router = express.Router();
 import { routes } from "./routes";
 
@@ -9,16 +9,13 @@ import { routes } from "./routes";
 function authRouter(app: any, Sentry: any) {
   app.use(
     "/api",
-    // (req: any, res: any, next: any) => {
-    //   console.log(`${req.method} ${req.originalUrl} from ${req.ip}`);
-    //   next();
-    // },
+    (req: Request, res: Response, next: NextFunction) => {
+      console.log(`${req.method} ${req.originalUrl} from ${req.ip}`);
+      next();
+    },
     routes
   );
-  // router.get("/debug-sentry", function mainHandler(req, res) {
-  //   throw new Error("My first Sentry error!");
-  // });
-console.log("test")
+
   router.get("/current-version", function mainHandler(req, res) {
     const { version } = require("../../../package.json");
     return { version };
@@ -30,7 +27,7 @@ console.log("test")
   // router.get("/", (req, res, next) => res.json({ index: "/" }));
 
 
-  // app.use((err, req, res, next) => {
+  // app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   //   globalExceptionHandlerController.handleException(err, req, res, next);
   // });
   app.use(Sentry.Handlers.errorHandler());
